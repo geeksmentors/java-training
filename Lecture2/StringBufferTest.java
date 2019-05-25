99b741a5ca1d9881351eeb43ed08833aed8a2dc5
Shashi Bhushan Kumar <shashibk1993@gmail.com>
2019-05-26 00:30:37 +0530

    Update StringBufferTest.java

--- Lecture2/StringBufferTest.java
+++ Lecture2/StringBufferTest.java
@@ -3,11 +3,7 @@ class StringBufferTest {
     public static void main(String[] args)
     {
         StringBuffer s = new StringBuffer("GeeksforGeeks");
-        int p = s.length();
-        int q = s.capacity();
-        System.out.println("Length of string GeeksforGeeks=" + p);
-        System.out.println("Capacity of string GeeksforGeeks=" + q);
-
+       System.out.println(s);
 
         StringBuffer s2 = new StringBuffer("Geeksfor");
         s2.append("Geeks");
