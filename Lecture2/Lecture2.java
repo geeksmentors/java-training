e22b948abb848aaae11082d9017b9bd3bfb15604
Shashi Bhushan Kumar <shashibk1993@gmail.com>
2019-05-26 10:58:27 +0530

    Update Lecture2.java

--- Lecture2/Lecture2.java
+++ Lecture2/Lecture2.java
@@ -31,3 +31,31 @@
 
 ===================
   
+  public class AnimalManager {
+
+    public void makeAnimalSound(Animal animal)
+    {
+        animal.sound();
+    }
+}
+
+
+=====
+  
+  public class Overriding {
+
+    public static void main(String[] args) {
+        Lion lion1 = new Lion();
+        lion1.sound();
+
+        Cat cat1 = new Cat();
+        cat1.sound();
+
+        AnimalManager animalManager = new AnimalManager();
+        animalManager.makeAnimalSound(cat1);
+        animalManager.makeAnimalSound(lion1);
+
+    }
+}
+
+  
