493df633e965377fd79f67bc0a869d66a9c5bd01
Shashi Bhushan Kumar <shashibk1993@gmail.com>
2019-06-02 11:09:55 +0530

    Update Socket1.java

--- Socket1.java
+++ Socket1.java
@@ -93,3 +93,30 @@ public class GreetClient {
         System.out.println(response);
     }
 }
+
+
+=============================
+    
+    
+    package com.company;
+import javax.swing.*;
+
+public class Swing1 {
+
+        public static void main(String[] args) {
+            JFrame f=new JFrame();//creating instance of JFrame
+
+            JButton b=new JButton("click");//creating instance of JButton
+            b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+
+            f.add(b);//adding button in JFrame
+
+            f.setSize(400,500);//400 width and 500 height
+            f.setLayout(null);//using no layout managers
+            f.setVisible(true);//making the frame visible
+        }
+}
+
+
+
+=====================================
