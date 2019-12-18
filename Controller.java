043f0c664dcb317389e280160bf828e0d6ca27e8
Shashi Bhushan Kumar <shashibk1993@gmail.com>
2019-12-18 10:48:35 +0530

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,15 +1,19 @@
 package com.example.Spring3;
 
+import com.sun.research.ws.wadl.Representation;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.MediaType;
 
         import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.http.ResponseEntity;
         import org.springframework.web.bind.annotation.*;
 
         import javax.validation.Valid;
-        import java.util.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import java.util.*;
 
         import org.springframework.web.bind.annotation.RestController;
         import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +38,65 @@ public class Controller {
         user1.setName("Name");
         return user1;
     }*/
+
+    @GetMapping(path = "/hello/{name}")
+    public String helloWorldPathVariable(@PathVariable String name) {
+        String s1 = name;
+        return "Hello "+ s1;
+    }
+
+    @RequestMapping("/user")
+    public String getitem(@RequestParam("data") String itemid){
+
+        return "Hi " + itemid;
+    }
+    // http://localhost:8080/user?data=6
+
+
+    /*
+
+    @PostMapping("/addAUser")
+    @Consumes{MediaType.APPLICATION_JSON,MediaType.APPLICATION_FORM_URLENCODED_VALUE}
+    @Produces(MediaType.APPLICATION_JSON)
+    User newEmployee(@RequestBody User user) {
+        return user;
+    }
+
+     */
+    @PostMapping(path = "/users", consumes = "application/json", produces = "application/json")
+    public User addMember(@RequestBody User user) {
+        System.out.println("Post call for Use Creation");
+        return user;
+    }
+
+    @PutMapping(path="/user/{userId}",consumes={MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE} )
+    public User updateUser(@PathVariable String userId, @RequestBody User user)    {
+        user.setId(userId);
+        return user;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public boolean deleteEmployee(@PathVariable(value = "id") Long employeeId)
+            {
+                System.out.println("Delete call for Use Creation"+ employeeId);
+        return true;
+    }
+
+
+    /*
+    @RequestMapping(value = "/user", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public @ResponseBody  String authenticate(@PathVariable("email") String anEmailAddress, MultiValueMap paramMap) throws Exception {
+        if(paramMap == null && paramMap.get("password") == null) {
+            throw new IllegalArgumentException("Password not provided");
+        }
+        return "Done, user Added";
+    }
+
+     */
+
 }
 /*
 @RestController
@@ -52,20 +115,4 @@ public class Controller {
 }
 
  */
-
- @GetMapping(path = "/hello/{name}")
-    public String helloWorldPathVariable(@PathVariable String name) {
-        String s1 = name;
-        return "Hello "+ s1;
-    }
-
-=========================
-        
-            @RequestMapping("/user")
-    public String getitem(@RequestParam("data") String itemid){
-
-        return "Hi " + itemid;
-    }
-    // http://localhost:8080/user?data=6
-        
 
